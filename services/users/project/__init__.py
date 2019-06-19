17b39bb81a740e34545b054f97904f85abd542eb
Kah Wai LIM <kahwai222@gmail.com>
2019-06-19 17:06:42 +0800

    Part 1 Chapter 7

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,7 @@
 import os
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 
 # instantiate the app
@@ -14,6 +15,22 @@ api = Api(app)
 # set config
 app_settins = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settins)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
