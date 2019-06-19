be55351e2a551b23fd81edb2bbed97f259e9d1b0
Kah Wai LIM <kahwai222@gmail.com>
2019-06-19 17:59:13 +0800

    Part 1 Chapter 8

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,6 +1,9 @@
 # services/users/manage.py
 
 
+import sys
+import unittest
+
 from flask.cli import FlaskGroup
 
 from project import app, db
@@ -14,6 +17,16 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 
 if __name__ == '__main__':
