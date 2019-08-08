e5b5776eec46018120f7d0b07a2a5101f0c63d1d
Kah Wai LIM <kahwai222@gmail.com>
2019-08-08 10:27:00 +0800

    Part 3 Chapter 12

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -83,7 +83,8 @@ class UsersList(Resource):
             'message': 'Invalid payload.'
         }
         if not is_admin(resp):
-            response_object['message'] = 'You do not have permission to do that.'
+            response_object['message'] = \
+                'You do not have permission to do that.'
             return response_object, 401
         if not post_data:
             return response_object, 400
