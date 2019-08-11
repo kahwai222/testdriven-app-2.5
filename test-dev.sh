8213c49121d94bb9c6469237586a43d486639134
Kah Wai LIM <kahwai222@gmail.com>
2019-08-11 17:43:40 +0800

    Part 4 Chapter 7

--- test-dev.sh
+++ test-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+type=$1
 fails=""
 
 inspect() {
@@ -9,31 +10,62 @@ inspect() {
   fi
 }
 
-# run unit and integration tests
-docker-compose up -d --build
-winpty docker-compose exec users python manage.py test
-inspect $? users
-winpty docker-compose exec users flake8 project
-inspect $? users-lint
-winpty docker-compose exec client npm run coverage
-inspect $? client
-docker-compose down
+# run server-side tests
+server() {
+  docker-compose up -d --build
+  winpty docker-compose exec users python manage.py test
+  inspect $? users
+  winpty docker-compose exec users flake8 project
+  inspect $? users-lint
+  docker-compose down
+}
+
+# run client-side tests
+client() {
+  docker-compose up -d --build
+  winpty docker-compose exec client npm test -- --coverage
+  inspect $? client
+  docker-compose down
+}
 
 # run e2e tests
-docker-compose -f docker-compose-prod.yml up -d --build
-sleep 1
-winpty docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://localhost
-inspect $? e2e
-docker-compose -f docker-compose-prod.yml down
+e2e() {
+  docker-compose -f docker-compose-prod.yml up -d --build
+  sleep 2
+  winpty docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  # ./node_modules/.bin/cypress run --config baseUrl=http://192.168.99.100  # on Docker Toolbox
+  inspect $? e2e
+  docker-compose -f docker-compose-prod.yml down
+}
 
-## run e2e tests Docker Toolbox
-#docker-compose up -d --build
-#sleep 1
-#winpty docker-compose exec users python manage.py recreate_db
-#./node_modules/.bin/cypress run --config baseUrl=http://192.168.99.100
-#inspect $? e2e
-#docker-compose down
+# run all tests
+all() {
+  docker-compose up -d --build
+  winpty docker-compose exec users python manage.py test
+  inspect $? users
+  winpty docker-compose exec users flake8 project
+  inspect $? users-lint
+  winpty docker-compose exec client npm run coverage
+  inspect $? client
+  docker-compose down
+  e2e
+}
+
+# run appropriate tests
+if [[ "${type}" == "server" ]]; then
+  echo "Running server-side tests!"
+  server
+elif [[ "${type}" == "client" ]]; then
+  echo "Running client-side tests!"
+  client
+elif [[ "${type}" == "e2e" ]]; then
+  echo "Running e2e tests!"
+  e2e
+else
+  echo "Running all tests!"
+  all
+fi
 
 # return proper code
 if [ -n "${fails}" ]; then
